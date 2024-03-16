84c17205f2bc35cee6b72c9f3e7440fcdd2cedf9
Taruna Makky Satya Nugraha <tarunamakki@gmail.com>
2024-03-16 22:13:56 +0700

    Taruna Makky Satya Nugraha : CRUD Categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,48 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index() {
-        return view('category.index');
+        $category = DB::table('category') -> get();
+        return view('category.index', ['category' => $book]);
+    }
+
+    public function create() {
+        return view('category.create');
+    }
+
+    public function store(Request $request) {
+        $request -> validate([
+            'name' => 'required'
+        ]);
+
+        DB::table('category')->insert([
+            'name' => $request['name']
+        ]);
+        return redirect('/category');
+    }
+
+    public function show($id) {
+        $category = DB::table('category') -> where('id', $id) -> first();
+        return view('category.detail', ['category' => $category]);
+    }
+
+    public function edit($id) {
+        $category = DB::table('category') -> find($id);
+        return view('category.edit', ['category' => $category]);
+    }
+
+    public function update($id, Request $request) {
+        $request -> validate([
+            'name' => 'required'
+        ]);
+
+        DB::table('category')-> where('id', $id) -> update([
+            'name' => $request['name']
+        ]);
+        return redirect('/category');
+    }
+
+    public function destroy($id) {
+        DB::table('category') -> where('id', $id) -> delete();
+        return redirect('/category');
     }
 }
